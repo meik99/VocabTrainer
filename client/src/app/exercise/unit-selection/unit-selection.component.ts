ed9f07aff6d171d9aba96b1a4deacec6cd9fce13
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2017-01-19 14:10:20 +0100

    Implemented some unit selection logic

--- client/src/app/exercise/unit-selection/unit-selection.component.ts
+++ client/src/app/exercise/unit-selection/unit-selection.component.ts
@@ -7,23 +7,28 @@ import {Component, OnInit, Output} from '@angular/core';
 })
 export class UnitSelectionComponent implements OnInit {
 
-  schooltypes = [
+  private schooltypes = [
     { id:1, name: "HTL" },
     { id:2, name: "HAK" },
     { id:3 ,name: "AHS" }
     ];
 
-  schoollevel = [
+  private schoollevel = [
     { id: 1, typeId: 1, name: "1. Jahrgang" },
     { id: 2, typeId: 1, name: "2. Jahrgang" },
-    { id: 3, typeId: 2, name: "1. Jahrgang" } ,
-    { id: 4, typeId: 2, name: "2. Jahrgang" }
+    { id: 3, typeId: 2, name: "1. Kindergarten" } ,
+    { id: 4, typeId: 2, name: "2. Kindergarten" },
+    { id: 5, typeId: 3, name: "1. Klasse" } ,
+    { id: 6, typeId: 3, name: "2. Klasse" }
     ];
 
-  languages: [
-    { id: 1, name: "Deutsch" },
-    { id: 2, name: "Englisch" },
-    { id: 3, name: "Spanisch" }
+  private languages = [
+    { id: 1, typeId: 1, levelId: 1, name: "Deutsch" },
+    { id: 2, typeId: 1, levelId: 1,  name: "Englisch" },
+    { id: 1, typeId: 1, levelId: 2, name: "Deutsch" },
+    { id: 2, typeId: 1, levelId: 2,  name: "Englisch" },
+    { id: 2, typeId: 1, levelId: 2,  name: "Spanisch" },
+    { id: 3, typeId: 2, levelId: 3, name: "Spanisch" }
     ];
 
   units:[
@@ -36,6 +41,18 @@ export class UnitSelectionComponent implements OnInit {
   selectedInputLanguage;
   selectedOutputLanguage;
   selectedUnit;
+
+  getSchooltypes(){
+    return this.schooltypes;
+  }
+
+  getSchoollevel(typeId : number){
+    return this.schoollevel.filter(level => level.typeId == typeId);
+  }
+
+  getLanguages(typeId: number, levelId: number){
+    return this.languages.filter(language => language.typeId == typeId && language.levelId == levelId);
+  }
 
   constructor() { }
 
